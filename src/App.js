4e2d647ab25852ab60f08e884044f97a6ff5e7ff
Qiyuan Chen <qiyuan.chen2002@gmail.com>
2021-12-07 13:29:03 +0000

    Basic auth idea and setting up scss files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './dist/Main.css';
+
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyACBQxVSK5k4yt1sPDcy0ZAeXASDgmR0_A",
+  authDomain: "qchen-chat.firebaseapp.com",
+  projectId: "qchen-chat",
+  storageBucket: "qchen-chat.appspot.com",
+  messagingSenderId: "948407960054",
+  appId: "1:948407960054:web:1f0dcdd0ccf31741da07d7",
+  measurementId: "G-42XSYPX1V3"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+auth.onAuthStateChanged(user => {
+  if (user) {
+    console.log("Signed in");
+  } else {
+    console.log("Signed out");
+  }
+})
 
 function App() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => setCurrentUser(user));
+  }, []);
+
+  const signUp = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      setError(e.message);
+    }
+    setEmail("");
+    setPassword("");
+  }
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password); 
+    } catch (e) {
+      setError(e.message);
+    }
+    setEmail("");
+    setPassword("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={signUp}>
+        <label for="email">Email: </label>
+        <input type="text" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <label for="password">Password: </label>
+        <input type="password" name="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <p>{error}</p>
+        <button type="submit">Submit</button>
+      </form>
+
+      <form onSubmit={login}>
+        <label for="email">Email: </label>
+        <input type="text" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <label for="password">Password: </label>
+        <input type="password" name="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <p>{error}</p>
+        <button type="submit">Submit</button>
+      </form>
+
+      <button onClick={() => auth.signOut()}>Sign out</button>
     </div>
-  );
+  )
 }
 
 export default App;
